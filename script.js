79c7fd727654276975e1d3b32d999b03bd88724a
odinstudent <34312554+Alpattex@users.noreply.github>
2023-10-04 17:47:00 +0300

    Adding Event Listeners for Form and Table

--- script.js
+++ script.js
@@ -40,3 +40,40 @@ function renderExpenses() {
         localStorage.setItem("expenses", 
             JSON.stringify(expenses));
     }
+
+    // Function to add expense
+function addExpense(event) {
+    event.preventDefault();
+  
+    // Get expense name and amount from form
+    const expenseNameInput =
+        document.getElementById("expense-name");
+    const expenseAmountInput =
+        document.getElementById("expense-amount");
+    const expenseName =
+        expenseNameInput.value;
+    const expenseAmount =
+        parseFloat(expenseAmountInput.value);
+  
+    // Clear form inputs
+    expenseNameInput.value = "";
+    expenseAmountInput.value = "";
+  
+    // Validate inputs
+    if (expenseName === "" || isNaN(expenseAmount)) {
+        alert("Please enter valid expense details.");
+        return;
+    }
+  
+    // Create new expense object
+    const expense = {
+        name: expenseName,
+        amount: expenseAmount,
+    };
+  
+    // Add expense to expenses array
+    expenses.push(expense);
+  
+    // Render expenses
+    renderExpenses();
+}
